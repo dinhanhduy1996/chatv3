caf38ed80a8c6ad31833d86a821a91e4425d2131
anhduy <bachtuhoa@gmail.com>
2022-03-21 23:02:25 +0700

    alo

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app  = express();
+const server  = require('http').createServer(app);
+const io = require('socket.io')(server, {  
+    cors:  {origin: "*"}
+});
+
+
+
+//Khởi tao kết nối với MySQL Server
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "chat" 
+});
+        //Tiến hàng kết nối
+       
+        con.connect(function(err) {
+          if (err) throw err;
+          //Kết nôi thành công
+          console.log("Connected!");
+          //Tiến hành khởi tạo databse
+ 
+
+            io.on('connection',(socket)=>{
+
+              socket.on('chat message', function(msg){
+                  
+                  
+              
+          });
+
+          });
+        });
+
+const PORT = process.env.PORT||80;
+server.listen(80,()=>{
+    console.log('server is running');
+});
