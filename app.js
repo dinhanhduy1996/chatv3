0fa687be310cf18aae9ccca834a3a311e4e20891
dinhanhduy1996 <96035586+dinhanhduy1996@users.noreply.github.com>
2022-03-26 16:16:27 +0700

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const io = require('socket.io')(server, {
             io.on('connection',(socket)=>{
 
               socket.on('chat message', function(msg){
-                  
+                  io.emit('chat message', msg);
                   
               
           });
